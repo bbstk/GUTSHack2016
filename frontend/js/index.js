935fddd097460311d456d92d2548f49f883e612d
Ivaylo Lafchiev <ivailolafchiev@yahoo.com>
2016-10-15 15:12:41 +0100

    MY COMMIT IS BEST

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,75 +1,16 @@
-    var gauge1 = loadLiquidFillGauge("fillgauge1", 55);
+    var gauge1 = loadLiquidFillGauge("fillgauge1", 26, config1);
     var config1 = liquidFillGaugeDefaultSettings();
-    config1.circleColor = "#FF7777";
-    config1.textColor = "#FF4444";
-    config1.waveTextColor = "#FFAAAA";
-    config1.waveColor = "#FFDDDD";
-    config1.circleThickness = 0.2;
-    config1.textVertPosition = 0.2;
-    config1.waveAnimateTime = 1000;
-        var gauge2 = loadLiquidFillGauge("fillgauge2", 55);
-    var config2 = liquidFillGaugeDefaultSettings();
-    config1.circleColor = "#FF7777";
-    config1.textColor = "#FF4444";
-    config1.waveTextColor = "#FFAAAA";
-    config1.waveColor = "#FFDDDD";
-    config1.circleThickness = 0.2;
-    config1.textVertPosition = 0.2;
-    config1.waveAnimateTime = 1000;
-        var gauge3 = loadLiquidFillGauge("fillgauge3", 55);
-    var config3 = liquidFillGaugeDefaultSettings();
-    config1.circleColor = "#FF7777";
-    config1.textColor = "#FF4444";
-    config1.waveTextColor = "#FFAAAA";
-    config1.waveColor = "#FFDDDD";
-    config1.circleThickness = 0.2;
-    config1.textVertPosition = 0.2;
-    config1.waveAnimateTime = 1000;
-        var gauge4 = loadLiquidFillGauge("fillgauge4", 55);
-    var config4 = liquidFillGaugeDefaultSettings();
-    config1.circleColor = "#FF7777";
-    config1.textColor = "#FF4444";
-    config1.waveTextColor = "#FFAAAA";
-    config1.waveColor = "#FFDDDD";
-    config1.circleThickness = 0.2;
-    config1.textVertPosition = 0.2;
-    config1.waveAnimateTime = 1000;
-    //     var gauge5 = loadLiquidFillGauge("fillgauge5", 55);
-    // var config5 = liquidFillGaugeDefaultSettings();
-    // config1.circleColor = "#FF7777";
-    // config1.textColor = "#FF4444";
-    // config1.waveTextColor = "#FFAAAA";
-    // config1.waveColor = "#FFDDDD";
-    // config1.circleThickness = 0.2;
-    // config1.textVertPosition = 0.2;
-    // config1.waveAnimateTime = 1000;
-    //     var gauge6 = loadLiquidFillGauge("fillgauge6", 55);
-    // var config6 = liquidFillGaugeDefaultSettings();
-    // config1.circleColor = "#FF7777";
-    // config1.textColor = "#FF4444";
-    // config1.waveTextColor = "#FFAAAA";
-    // config1.waveColor = "#FFDDDD";
-    // config1.circleThickness = 0.2;
-    // config1.textVertPosition = 0.2;
-    // config1.waveAnimateTime = 1000;
-    //     var gauge7 = loadLiquidFillGauge("fillgauge7", 55);
-    // var config7 = liquidFillGaugeDefaultSettings();
-    // config1.circleColor = "#FF7777";
-    // config1.textColor = "#FF4444";
-    // config1.waveTextColor = "#FFAAAA";
-    // config1.waveColor = "#FFDDDD";
-    // config1.circleThickness = 0.2;
-    // config1.textVertPosition = 0.2;
-    // config1.waveAnimateTime = 1000;
-    //     var gauge8 = loadLiquidFillGauge("fillgauge8", 55);
-    // var config8 = liquidFillGaugeDefaultSettings();
-    // config1.circleColor = "#FF7777";
-    // config1.textColor = "#FF4444";
-    // config1.waveTextColor = "#FFAAAA";
-    // config1.waveColor = "#FFDDDD";
-    // config1.circleThickness = 0.2;
-    // config1.textVertPosition = 0.2;
-    // config1.waveAnimateTime = 1000;
+    config1.circleColor = "#FF0000";
+    config1.textColor = "#FF0000";
+    config1.waveTextColor = "#FF0000";
+    config1.waveColor = "#FF0000";
+    var gauge2 = loadLiquidFillGauge("fillgauge2", 12, config1);
+    var gauge3 = loadLiquidFillGauge("fillgauge3", 0);
+    var gauge4 = loadLiquidFillGauge("fillgauge4", 0);
+    var gauge5 = loadLiquidFillGauge("fillgauge5", 0);
+    var gauge6 = loadLiquidFillGauge("fillgauge6", 0);
+    var gauge7 = loadLiquidFillGauge("fillgauge7", 0);
+    var gauge8 = loadLiquidFillGauge("fillgauge8", 0);
     function NewValue(){
         if(Math.random() > .5){
             return Math.round(Math.random()*100);
@@ -80,6 +21,22 @@
 
 
     var data = [{"id":3,"teamid":1,"interest":12,"engagement":55,"stress":20,"relaxation":54,"focus":23},{"id":2,"teamid":1,"interest":33,"engagement":12,"stress":88,"relaxation":3,"focus":23}]
-    console.log(d3.select('#rowContainer').select('#row1').selectAll('svg').data(data).enter().append('p').text('hello'));
 
+
+    function neshtosi(test) {
+        updateCircles(test.id);
+        updateChart(test.id);
+
+    }
+
+    function updateCircles(id) {
+        if (test.id === 'focus') {
+            for (var i = 1; i <= 8; i++) {
+
+
+            }
+        }
+        console.log(test);
+        console.log(test.id);
+    }
 
